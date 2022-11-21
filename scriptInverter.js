4d26a9ee4efbbc51c1fed6ecdd7fecc99324a995
RandomlyFish <tobias.rheindorf@gmail.com>
2022-11-22 00:43:40 +0100

    Added script to invert csv haptic files

---
+++ scriptInverter.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+let data = fs.readFileSync("input.csv", "utf8");
+
+let lines = data.split("\n");
+
+for (let i = 1; i < lines.length; i++) {
+    let parts = lines[i].split(",");
+    lines[i] = parts[0] + "," + (100 - parseInt(parts[1]));
+}
+
+fs.writeFileSync("output.csv", lines.join("\n"));
